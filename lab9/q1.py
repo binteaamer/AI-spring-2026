e804923ae2bdea0564f346a8ae771f5738a1d174
abeeha binte aamer <abeehakhan50@gmail.com>
2026-04-15 18:28:30 +0500

    Add probability calculations for card drawing

---
+++ lab9/q1.py
@@ -0,0 +1,9 @@
+Task 1: Probability of Cards
+Using standard probability for a deck of 52 cards:
+
+Probability of drawing a red card (hearts or diamonds): There are 26 red cards.  P(Red) = 26/52 = 0.5
+Probability that the card is a heart, given it is red: There are 13 hearts in the 26 red cards.  P(Heart | Red) = 13/26 = 0.5
+Probability that a face card is a diamond: There are 12 face cards (J, Q, K of 4 suits) and 3 are diamonds.  P(Diamond | Face) = 3/12 = 0.25
+Probability that a face card is a spade or a queen: There are 12 face cards. 3 are spades (J, Q, K) and 3 additional queens (Hearts, Diamonds, Clubs). 
+Queen of Spades already counted.  
+Total favorable = 6.  P(Spade or Queen | Face) = 6/12 = 0.5
